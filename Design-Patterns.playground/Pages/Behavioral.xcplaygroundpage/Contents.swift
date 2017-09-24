0037b91459a15c214beadaadfffdf90f0c6a1197
remlostime <remlostime@gmail.com>
2017-09-23 18:14:15 -0700

    Format
    Make the indent correct for state pattern

--- Design-Patterns.playground/Pages/Behavioral.xcplaygroundpage/Contents.swift
+++ Design-Patterns.playground/Pages/Behavioral.xcplaygroundpage/Contents.swift
@@ -506,58 +506,58 @@ testChambers.testChamberNumber += 1
 >**Further Examples:** [Design Patterns in Swift](https://github.com/kingreza/Swift-Observer)
 */
 /*:
-🐉 State
----------
-
-The state pattern is used to alter the behaviour of an object as its internal state changes.
-The pattern allows the class for an object to apparently change at run-time.
-
-### Example
-*/
+ 🐉 State
+ ---------
+ 
+ The state pattern is used to alter the behaviour of an object as its internal state changes.
+ The pattern allows the class for an object to apparently change at run-time.
+ 
+ ### Example
+ */
 final class Context {
-	private var state: State = UnauthorizedState()
-
-    var isAuthorized: Bool {
-        get { return state.isAuthorized(context: self) }
-    }
-
-    var userId: String? {
-        get { return state.userId(context: self) }
-    }
-
-	func changeStateToAuthorized(userId: String) {
-		state = AuthorizedState(userId: userId)
-	}
-
-	func changeStateToUnauthorized() {
-		state = UnauthorizedState()
-	}
-
+  private var state: State = UnauthorizedState()
+  
+  var isAuthorized: Bool {
+    get { return state.isAuthorized(context: self) }
+  }
+  
+  var userId: String? {
+    get { return state.userId(context: self) }
+  }
+  
+  func changeStateToAuthorized(userId: String) {
+    state = AuthorizedState(userId: userId)
+  }
+  
+  func changeStateToUnauthorized() {
+    state = UnauthorizedState()
+  }
+  
 }
 
 protocol State {
-	func isAuthorized(context: Context) -> Bool
-	func userId(context: Context) -> String?
+  func isAuthorized(context: Context) -> Bool
+  func userId(context: Context) -> String?
 }
 
 class UnauthorizedState: State {
-	func isAuthorized(context: Context) -> Bool { return false }
-
-	func userId(context: Context) -> String? { return nil }
+  func isAuthorized(context: Context) -> Bool { return false }
+  
+  func userId(context: Context) -> String? { return nil }
 }
 
 class AuthorizedState: State {
-	let userId: String
-
-	init(userId: String) { self.userId = userId }
-
-	func isAuthorized(context: Context) -> Bool { return true }
-
-	func userId(context: Context) -> String? { return userId }
+  let userId: String
+  
+  init(userId: String) { self.userId = userId }
+  
+  func isAuthorized(context: Context) -> Bool { return true }
+  
+  func userId(context: Context) -> String? { return userId }
 }
 /*:
-### Usage
-*/
+ ### Usage
+ */
 let userContext = Context()
 (userContext.isAuthorized, userContext.userId)
 userContext.changeStateToAuthorized(userId: "admin")
@@ -565,8 +565,8 @@ userContext.changeStateToAuthorized(userId: "admin")
 userContext.changeStateToUnauthorized()
 (userContext.isAuthorized, userContext.userId)
 /*:
->**Further Examples:** [Design Patterns in Swift](https://github.com/kingreza/Swift-State)
-*/
+ >**Further Examples:** [Design Patterns in Swift](https://github.com/kingreza/Swift-State)
+ */
 /*:
 💡 Strategy
 -----------
--- README.md
+++ README.md
@@ -588,61 +588,61 @@ testChambers.testChamberNumber += 1
 
 ```
 
-🐉 State
----------
-
-The state pattern is used to alter the behaviour of an object as its internal state changes.
-The pattern allows the class for an object to apparently change at run-time.
-
-### Example
-
+ 🐉 State
+ ---------
+ 
+ The state pattern is used to alter the behaviour of an object as its internal state changes.
+ The pattern allows the class for an object to apparently change at run-time.
+ 
+ ### Example
+ 
 ```swift
 
 final class Context {
-	private var state: State = UnauthorizedState()
-
-    var isAuthorized: Bool {
-        get { return state.isAuthorized(context: self) }
-    }
-
-    var userId: String? {
-        get { return state.userId(context: self) }
-    }
-
-	func changeStateToAuthorized(userId: String) {
-		state = AuthorizedState(userId: userId)
-	}
-
-	func changeStateToUnauthorized() {
-		state = UnauthorizedState()
-	}
-
+  private var state: State = UnauthorizedState()
+  
+  var isAuthorized: Bool {
+    get { return state.isAuthorized(context: self) }
+  }
+  
+  var userId: String? {
+    get { return state.userId(context: self) }
+  }
+  
+  func changeStateToAuthorized(userId: String) {
+    state = AuthorizedState(userId: userId)
+  }
+  
+  func changeStateToUnauthorized() {
+    state = UnauthorizedState()
+  }
+  
 }
 
 protocol State {
-	func isAuthorized(context: Context) -> Bool
-	func userId(context: Context) -> String?
+  func isAuthorized(context: Context) -> Bool
+  func userId(context: Context) -> String?
 }
 
 class UnauthorizedState: State {
-	func isAuthorized(context: Context) -> Bool { return false }
-
-	func userId(context: Context) -> String? { return nil }
+  func isAuthorized(context: Context) -> Bool { return false }
+  
+  func userId(context: Context) -> String? { return nil }
 }
 
 class AuthorizedState: State {
-	let userId: String
-
-	init(userId: String) { self.userId = userId }
-
-	func isAuthorized(context: Context) -> Bool { return true }
-
-	func userId(context: Context) -> String? { return userId }
+  let userId: String
+  
+  init(userId: String) { self.userId = userId }
+  
+  func isAuthorized(context: Context) -> Bool { return true }
+  
+  func userId(context: Context) -> String? { return userId }
 }
 ```
 
-### Usage
-
+ ### Usage
+ 
 ```swift
 
 let userContext = Context()
@@ -653,8 +653,8 @@ userContext.changeStateToUnauthorized()
 (userContext.isAuthorized, userContext.userId)
 ```
 
->**Further Examples:** [Design Patterns in Swift](https://github.com/kingreza/Swift-State)
-
+ >**Further Examples:** [Design Patterns in Swift](https://github.com/kingreza/Swift-State)
+ 
 ```swift
 
 ```
